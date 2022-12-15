a74a0d4a1b2a8a2675ce4e39af874451ce54596b
Michael Westblade <michaelwestblade@gmail.com>
2022-12-14 20:42:55 -0600

    another custom hook

--- hooks/src/SimpleFormHooks.tsx
+++ hooks/src/SimpleFormHooks.tsx
@@ -2,10 +2,14 @@ import { ChangeEvent, useState } from 'react';
 import { useInputState } from './hooks/useInputState';
 
 export const SimpleFormHooks = () => {
-  const [email, setEmail] = useInputState("");
+  const [email, setEmail, resetEmail] = useInputState("");
+  const [password, setPassword, resetPassword] = useInputState("");
   return <div>
     <h1>The value is...</h1>
     <input type="text" value={email} onChange={setEmail}/>
-    <pre>{email}</pre>
+    <input type="text" value={password} onChange={setPassword}/>
+    <button onClick={resetEmail}>Reset email</button>
+    <button onClick={resetPassword}>Reset Password</button>
+    <pre>EMAIL: {email} & PASSWORD: {password}</pre>
   </div>
 }
